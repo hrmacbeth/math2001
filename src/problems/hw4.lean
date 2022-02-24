688c581d5b1db06c2d54cf0b662e9ad3ed70d747
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-02-23 19:20:01 -0500

    fix hw4 submission problem

--- src/problems/hw4.lean
+++ src/problems/hw4.lean
@@ -4,40 +4,35 @@ import tactics.push_neg
 import data.int.parity
 import data.real.sqrt
 
-
-/- problem 1 -/
-example (a : ℤ) : odd (a ^ 2 + 4 * a + 5) ↔ even a :=
+lemma problem1 (a : ℤ) : odd (a ^ 2 + 4 * a + 5) ↔ even a :=
 begin
   split,
   { sorry, },
   { sorry, },
 end
 
-/- problem 2 -/
-example (n : ℤ) : 7 ∣ 10 * n ↔ 7 ∣ n :=
+lemma problem2 (n : ℤ) : 7 ∣ 10 * n ↔ 7 ∣ n :=
 begin
   split,
   { sorry, },
   { sorry },
 end
 
-/- problem 3; the lemma `mul_eq_zero` may be useful -/
-example (x y : ℝ) : x ^ 3 + x ^ 2 * y = y ^ 2 + x * y ↔ y = x ^ 2 ∨ y = - x :=
+/- the lemma `mul_eq_zero` may be useful -/
+lemma problem3 (x y : ℝ) : x ^ 3 + x ^ 2 * y = y ^ 2 + x * y ↔ y = x ^ 2 ∨ y = - x :=
 begin
   split,
   { sorry, },
   { sorry, }
 end
 
-/- problem 7; the lemma `real.lt_sqrt` may be useful -/
-example : ∃ x > 0, x ^ 2 < real.sqrt x :=
+/- the lemma `real.lt_sqrt` may be useful -/
+lemma problem7 : ∃ x > 0, x ^ 2 < real.sqrt x :=
 begin
   sorry
 end
 
-/- problem 8 -/
-example : ∃ n : ℕ, 11 ∣ 2 ^ n - 1 :=
+lemma problem8 : ∃ n : ℕ, 11 ∣ 2 ^ n - 1 :=
 begin
   sorry
 end
-
