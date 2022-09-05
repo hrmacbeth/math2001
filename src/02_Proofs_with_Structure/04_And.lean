8553793c73094220aae16490aa152f30fef52e14
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2022-09-05 12:14:34 -0400

    Update Mon Sep  5 12:14:34 EDT 2022

--- src/02_Proofs_with_Structure/04_And.lean
+++ src/02_Proofs_with_Structure/04_And.lean
@@ -18,10 +18,10 @@ end
 example {p : ℚ} (hp : p ^ 2 = 9) : p ≤ 5 :=
 begin
   have hp' : -3 ≤ p ∧ p ≤ 3,
-  apply abs_le_of_sq_le_sq',
-  calc p ^ 2 ≤ 9 : by addarith hp
-  ... = 3 ^ 2 : by norm_num,
-  norm_num,
+  { apply abs_le_of_sq_le_sq',
+    calc p ^ 2 ≤ 9 : by addarith hp
+    ... = 3 ^ 2 : by norm_num,
+    norm_num, },
   sorry,
 end
 
@@ -30,11 +30,11 @@ example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) :
   a = 9 ∧ b = 1 :=
 begin
   split,
-  calc a = 4 + 5 * b : by addarith h1
-  ... = -6 + 5 * (b + 2) : by ring
-  ... = -6 + 5 * 3 : by rw [h2]
-  ... = 9 : by ring,
-  addarith h2,
+  { calc a = 4 + 5 * b : by addarith h1
+    ... = -6 + 5 * (b + 2) : by ring
+    ... = -6 + 5 * 3 : by rw [h2]
+    ... = 9 : by ring, },
+  { addarith h2, },
 end
 
 
@@ -42,12 +42,12 @@ example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) :
   a = 9 ∧ b = 1 :=
 begin
   have hb : b = 1,
-  addarith h2,
+  { addarith h2 },
   split,
-  calc a = 4 + 5 * b : by addarith h1
-  ... = 4 + 5 * 1 : by rw [hb]
-  ... = 9 : by ring,
-  apply hb,
+  { calc a = 4 + 5 * b : by addarith h1
+    ... = 4 + 5 * 1 : by rw [hb]
+    ... = 9 : by ring, },
+  { apply hb, },
 end
 
 
@@ -63,6 +63,13 @@ end
 
 
 example {x y : ℚ} (h : x + y = 5 ∧ x + 2 * y = 7) : x = 3 ∧ y = 2 :=
+begin
+  sorry
+end
+
+
+example {a b : ℝ} (h1 : a * b = a) (h2 : a * b = b) :
+  (a = 0 ∧ b = 0) ∨ (a = 1 ∧ b = 1) :=
 begin
   sorry
 end
---
+++ src/02_Proofs_with_Structure/05_Exists.lean
@@ -0,0 +1,119 @@
+/- Copyright (c) Heather Macbeth, 2022.  All rights reserved. -/
+
+import data.real.sqrt
+import library.arithmetic
+import tactics.inequalities
+
+
+example {a : ℚ} (h : ∃ b : ℚ, a = b ^ 2 + 1) : a > 0 :=
+begin
+  cases h with b hb,
+  calc a = b ^ 2 + 1 : hb
+  ... > 0 : by ineq_tac [],
+end
+
+
+example {t : ℝ} (h : ∃ a : ℝ, a * t < 0) : t ≠ 0 :=
+begin
+  cases h with x hxt,
+  have H : x ≤ 0 ∨ 0 < x := le_or_gt x 0,
+  cases H with hx hx,
+  { apply ne_of_gt,
+    apply pos_of_mul_neg_right hxt hx, },
+  sorry,
+end
+
+
+example : ∃ n : ℤ, 12 * n = 84 :=
+begin
+  use 7,
+  norm_num,
+end
+
+
+example (x : ℝ) : ∃ y : ℝ, y > x :=
+begin
+  use x + 1,
+  ineq_tac [],
+end
+
+
+example : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 11 :=
+begin
+  sorry,
+end
+
+
+example (a : ℤ) : ∃ m n : ℤ, m ^ 2 - n ^ 2 = 2 * a + 1 :=
+begin
+  sorry,
+end
+
+
+example {p q : ℝ} (h : p < q) : ∃ x, p < x ∧ x < q :=
+begin
+  sorry,
+end
+
+
+example : ∃ a b c d : ℕ, a ^ 3 + b ^ 3 = 1729 ∧ c ^ 3 + d ^ 3 = 1729 ∧ a ≠ c ∧ a ≠ d :=
+begin
+  use [1, 12, 9, 10],
+  split,
+  norm_num,
+  split,
+  norm_num,
+  split,
+  norm_num,
+  norm_num,
+end
+
+
+example : ∃ t : ℚ, t ^ 2 = 1.69 :=
+begin
+  sorry,
+end
+
+
+example : ∃ m n : ℤ, m ^ 2 + n ^ 2 = 85 :=
+begin
+  sorry,
+end
+
+
+example : ∃ t : ℝ, t < 0 ∧ t ^ 2 < 1 :=
+begin
+  sorry,
+end
+
+
+example : ∃ a b : ℕ, 2 ^ a = 5 * b + 1 :=
+begin
+  sorry,
+end
+
+
+example (x : ℚ) : ∃ y : ℚ, y ^ 2 > x :=
+begin
+  sorry,
+end
+
+
+example {t : ℝ} (h : ∃ a : ℝ, a * t + 1 < a + t) : t ≠ 1 :=
+begin
+  sorry,
+end
+
+
+example {m : ℤ} (h : ∃ a, 2 * a = m) : m ≠ 5 :=
+begin
+  sorry,
+end
+
+
+example {a b c : ℝ} (ha : a ≤ b + c) (hb : b ≤ a + c) (hc : c ≤ a + b) 
+  (ha' : 0 ≤ a) (hb' : 0 ≤ b) (hc' : 0 ≤ c) :
+  ∃ x y z, x ≥ 0 ∧ y ≥ 0 ∧ z ≥ 0 ∧ a = y + z ∧ b = x + z ∧ c = x + y :=
+begin
+  sorry,
+end
