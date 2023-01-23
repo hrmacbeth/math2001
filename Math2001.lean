4a7ba41efbf9028521816a71bb239c09c69ef849
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-01-23 04:14:11 -0500

    Update Mon Jan 23 04:14:11 EST 2023

--- Math2001.lean
+++ Math2001.lean
@@ -2,6 +2,3 @@ import Math2001.Library.Arithmetic
 import Math2001.Tactic.Rel
 import Math2001.Tactic.Rel.Attr
 import Math2001.Tactic.Rel.Basic
-import Math2001.Test.Addarith
-import Math2001.Test.IneqRel
-import Math2001.Test.ModRel
