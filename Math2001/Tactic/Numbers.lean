66d81983706eb8b4929b3c6dbd7f65ab46446f78
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-01-23 13:43:41 -0500

    Update Mon Jan 23 13:43:41 EST 2023

---
+++ Math2001/Tactic/Numbers.lean
@@ -0,0 +1,14 @@
+import Mathlib.Tactic.NormNum
+
+open Lean hiding Rat mkRat
+open Lean.Meta Qq Lean.Elab Term
+open Lean.Parser.Tactic Mathlib.Meta.NormNum
+
+/--
+Normalize numerical expressions. Supports the operations `+` `-` `*` `/` `⁻¹` and `^`
+over numerical types such as `ℕ`, `ℤ`, `ℚ`, `ℝ`, `ℂ` and some general algebraic types,
+and can prove goals of the form `A = B`, `A ≠ B`, `A < B` and `A ≤ B`, where `A` and `B` are
+numerical expressions.
+-/
+elab (name := numbers) "numbers" loc:(location ?) : tactic =>
+  elabNormNum mkNullNode loc (simpOnly := true) (useSimp := false)
