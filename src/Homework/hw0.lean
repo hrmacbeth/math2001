cd479b70956912071404f97148a73280407f46ba
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-01-26 03:48:57 -0500

    Update Thu Jan 26 03:48:57 EST 2023

---
+++ src/Homework/hw0.lean
@@ -0,0 +1,10 @@
+/- Copyright (c) Heather Macbeth, 2022.  All rights reserved. -/
+import Math2001.Tactic.Numbers
+import Math2001.Tactic.Rel
+
+/-! # Homework 0 -/
+
+
+/- 5 points -/
+example {n : ℤ} (hn : n ≥ 5) : n ^ 2 > 2 * n + 11 :=
+  sorry
