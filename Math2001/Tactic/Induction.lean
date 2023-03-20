df0c4041def4e3e846718f3201f8887c89660d02
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-03-20 08:48:33 -0400

    Update Mon Mar 20 08:48:33 EDT 2023

--- Math2001/Tactic/Induction.lean
+++ Math2001/Tactic/Induction.lean
@@ -4,8 +4,10 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Heather Macbeth
 -/
 import Mathlib.Data.Nat.Basic
+import Mathlib.Data.Int.Basic
 import Mathlib.Tactic.NormCast
 import Mathlib.Tactic.Cases
+import Mathlib.Tactic.Have
 
 /-! # Specialized induction tactics
 
@@ -20,15 +22,59 @@ called pre-emptively on all goals).
 | k+1 => inductive_step k (Nat.recAux base_case inductive_step k)
 
 @[elab_as_elim]
-lemma Nat.induction {P : ℕ → Prop} (base_case : P 0)
-    (inductive_hypothesis : ∀ k, (IH : P k) → P (k + 1)) : (∀ n, P n) :=
-  Nat.rec base_case inductive_hypothesis
+theorem Nat.induction {P : ℕ → Prop} (base_case : P 0)
+    (inductive_step : ∀ k, (IH : P k) → P (k + 1)) : (∀ n, P n) :=
+  Nat.rec base_case inductive_step
 
 @[elab_as_elim]
 def Nat.two_step_induction' {P : ℕ → Sort u} (base_case_0 : P 0) (base_case_1 : P 1)
-    (inductive_hypothesis : ∀ (k : ℕ), (IH0 : P k) → (IH1 : P (k + 1)) → P (k + 1 + 1)) (a : ℕ) :
+    (inductive_step : ∀ (k : ℕ), (IH0 : P k) → (IH1 : P (k + 1)) → P (k + 1 + 1)) (a : ℕ) :
     P a :=
-  Nat.two_step_induction base_case_0 base_case_1 inductive_hypothesis a
+  Nat.two_step_induction base_case_0 base_case_1 inductive_step a
+
+@[elab_as_elim]
+theorem Int.induction_on' {p : ℤ → Prop} (base_case : p 0)
+    (forward_inductive_step : ∀ (i : ℤ) (hi : 0 ≤ i) (IH : p i),  p (i + 1))
+    (backward_inductive_step : ∀ (i : ℤ) (hi : i ≤ 0) (IH : p i), p (i - 1)) (i : ℤ) :
+    p i := by
+  refine Int.induction_on i base_case (fun i hi ↦ ?_) (fun i hi ↦ ?_)
+  · exact forward_inductive_step _ (Int.ofNat_nonneg i) hi
+  · exact backward_inductive_step _ (Int.neg_le_of_neg_le (Int.ofNat_nonneg i)) hi
+
+@[elab_as_elim]
+theorem Nat.strong_induction_succ {p : (n : ℕ) → Sort _} (base_case : p 0) 
+    (inductive_step : ∀ (k : ℕ), (∀ (m : ℕ), m ≤ k → p m) → p (k + 1)) :
+    ∀ n : ℕ, p n
+  | 0 => base_case
+  | k + 1 => by
+      induction' k using Nat.strong_rec_on with l IH
+      apply inductive_step
+      intro m hm
+      match m with
+      | 0 => apply base_case
+      | t + 1 => apply IH _ hm
+
+@[elab_as_elim]
+theorem Nat.strong_le_induction {s : ℕ} {p : (n : ℕ) → s ≤ n → Prop} 
+    (inductive_step : ∀ (k : ℕ) (hk : s ≤ k), (∀ (m : ℕ) (hm : s ≤ m), m < k → p m hm) → p k hk)
+    (n : ℕ) (hn : s ≤ n) : p n hn := by
+  let P : ℕ → Prop := fun (n : ℕ) ↦ ∀ (k : ℕ) (hk : s ≤ k), k ≤ n → p k hk
+  have h : ∀ (m : ℕ), s ≤ m → P m
+  · refine Nat.le_induction ?_ ?_
+    · show ∀ (k : ℕ) (hk : s ≤ k), k ≤ s → p k hk
+      intro k hk hk'
+      apply inductive_step
+      intro m hm hm'
+      have := not_lt_of_le <| hk'.trans hm
+      contradiction
+    intro k _ IH m hm hm'
+    apply inductive_step
+    intro l hl hl'
+    apply IH
+    apply le_of_lt_succ
+    exact lt_of_lt_of_le hl' hm'
+  apply h n hn
+  apply le_refl
 
 open Lean Parser Category Elab Tactic
 
@@ -54,4 +100,36 @@ syntax (name := TwoStepInductionSyntax) "two_step_induction " (casesTarget,+) ("
 macro_rules
 | `(tactic| two_step_induction $tgts,* $[with $withArg*]?) =>
     `(tactic| induction' $tgts,* using Nat.two_step_induction' $[with $withArg*]? <;>
+      push_cast (config := { decide := false }))
+
+open private getElimNameInfo generalizeTargets generalizeVars in evalInduction in
+syntax (name := IntInductionSyntax) "integer_induction " (casesTarget,+) (" with " (colGt binderIdent)+)? : tactic
+
+macro_rules
+| `(tactic| integer_induction $tgts,* $[with $withArg*]?) =>
+    `(tactic| induction' $tgts,* using Int.induction_on' $[with $withArg*]? <;>
+      push_cast (config := { decide := false }))
+
+open private getElimNameInfo generalizeTargets generalizeVars in evalInduction in
+syntax (name := StrongInductionSyntax) "strong_induction " (casesTarget,+) (" with " (colGt binderIdent)+)? : tactic
+
+macro_rules
+| `(tactic| strong_induction $tgts,* $[with $withArg*]?) =>
+    `(tactic| induction' $tgts,* using Nat.strong_induction_on $[with $withArg*]? <;>
+      push_cast (config := { decide := false }))
+
+open private getElimNameInfo generalizeTargets generalizeVars in evalInduction in
+syntax (name := StrongInductionOneSyntax) "strong_induction_from_one " (casesTarget,+) (" with " (colGt binderIdent)+)? : tactic
+
+macro_rules
+| `(tactic| strong_induction_from_one $tgts,* $[with $withArg*]?) =>
+    `(tactic| induction' $tgts,* using Nat.strong_induction_succ $[with $withArg*]? <;>
+      push_cast (config := { decide := false }))
+
+open private getElimNameInfo generalizeTargets generalizeVars in evalInduction in
+syntax (name := StartingPointStrongInductionSyntax) "strong_induction_from_starting_point " (casesTarget,+) (" with " (colGt binderIdent)+)? : tactic
+
+macro_rules
+| `(tactic| strong_induction_from_starting_point $tgts,* $[with $withArg*]?) =>
+    `(tactic| induction' $tgts,* using Nat.strong_le_induction $[with $withArg*]? <;>
       push_cast (config := { decide := false }))
---
+++ src/06_Induction/03_Two_Step_Induction.lean
@@ -0,0 +1,92 @@
+import Math2001.Library.Parity
+import Math2001.Tactic.Addarith
+import Math2001.Tactic.Induction
+import Math2001.Tactic.ModCases
+import Math2001.Tactic.Numbers
+import Math2001.Tactic.Rel
+import Math2001.Tactic.Take
+
+open Int
+
+
+def a : ℕ → ℤ 
+  | 0 => 2
+  | 1 => 1
+  | n + 2 => a (n + 1) + 2 * a n 
+
+
+#eval a 5 -- infoview displays `31`
+
+
+example (n : ℕ) : a n = 2 ^ n + (-1) ^ n := by 
+  two_step_induction n with k IH1 IH2
+  . calc a 0 = 2 := by rw [a]
+      _ = 2 ^ 0 + (-1) ^ 0 := by numbers
+  . calc a 1 = 1 := by rw [a]
+      _ = 2 ^ 1 + (-1) ^ 1 := by numbers
+  calc
+    a (k + 2) 
+      = a (k + 1) + 2 * a k := by rw [a]
+    _ = (2 ^ (k + 1) + (-1) ^ (k + 1)) + 2 * (2 ^ k + (-1) ^ k) := by rw [IH1, IH2]
+    _ = (2 : ℤ) ^ (k + 2) + (-1) ^ (k + 2) := by ring
+
+
+example {m : ℕ} (hm : 1 ≤ m) : a m ≡ 1 [ZMOD 6] ∨ a m ≡ 5 [ZMOD 6] := by
+  have H : ∀ n : ℕ, 1 ≤ n → 
+      (a n ≡ 1 [ZMOD 6] ∧ a (n + 1) ≡ 5 [ZMOD 6])
+    ∨ (a n ≡ 5 [ZMOD 6] ∧ a (n + 1) ≡ 1 [ZMOD 6]) 
+  · intro n hn
+    induction_from_starting_point n, hn with k hk IH
+    · left
+      constructor
+      calc a 1 = 1 := by rw [a]
+        _ ≡ 1 [ZMOD 6] := by extra
+      calc a (1 + 1) = 1 + 2 * 2 := by rw [a, a, a]
+        _ = 5 := by numbers
+        _ ≡ 5 [ZMOD 6] := by extra
+    · obtain ⟨IH1, IH2⟩ | ⟨IH1, IH2⟩ := IH
+      · right
+        constructor
+        · apply IH2 
+        calc a (k + 1 + 1) = a (k + 1) + 2 * a k := by rw [a]
+          _ ≡ 5 + 2 * 1 [ZMOD 6] := by rel [IH1, IH2] 
+          _ = 6 * 1 + 1 := by numbers
+          _ ≡ 1 [ZMOD 6] := by extra
+      · left
+        constructor
+        · apply IH2 
+        calc a (k + 1 + 1) = a (k + 1) + 2 * a k := by rw [a]
+          _ ≡ 1 + 2 * 5 [ZMOD 6] := by rel [IH1, IH2] 
+          _ = 6 * 1 + 5 := by numbers
+          _ ≡ 5 [ZMOD 6] := by extra
+  obtain ⟨H1, H2⟩ | ⟨H1, H2⟩ := H m hm
+  · left
+    apply H1
+  · right
+    apply H1
+
+
+def F : ℕ → ℤ 
+  | 0 => 1
+  | 1 => 1
+  | n + 2 => F (n + 1) + F n 
+
+
+example (n : ℕ) : F (n + 1) ^ 2 - F (n + 1) * F n - F n ^ 2 = - (-1) ^ n := by
+  simple_induction n with k IH
+  · calc F 1 ^ 2 - F 1 * F 0 - F 0 ^ 2 = 1 ^ 2 - 1 * 1 - 1 ^ 2 := by rw [F, F]
+      _ = - (-1) ^ 0 := by numbers
+  · calc F (k + 1 + 1) ^ 2 - F (k + 1 + 1) * F (k + 1) - F (k + 1) ^ 2
+        = (F (k + 1) + F k) ^ 2 - (F (k + 1) + F k) * F (k + 1) - F (k + 1) ^ 2 := by rw [F]
+      _ = - (F (k + 1) ^ 2 - F (k + 1) * F k - F k ^ 2) := by ring
+      _ = - -(-1) ^ k := by rw [IH]
+      _ = -(-1) ^ (k + 1) := by ring
+
+
+def b : ℕ → ℤ 
+  | 0 => 0
+  | 1 => 1
+  | n + 2 => 5 * b (n + 1) - 6 * b n 
+
+example (n : ℕ) : b n = 3 ^ n - 2 ^ n := by 
+  sorry
