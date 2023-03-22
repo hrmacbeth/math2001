a8b0b7baf13d9e7fc7d17d359404765f5ebfe490
y-samuel <33128102+samuelUniversal@users.noreply.github.com>
2023-03-22 13:54:36 +0800

    Update 02_Proving_Equalities_in_Lean.lean
    remove trailing spaces

--- src/01_Proofs_by_Calculation/02_Proving_Equalities_in_Lean.lean
+++ src/01_Proofs_by_Calculation/02_Proving_Equalities_in_Lean.lean
@@ -6,7 +6,7 @@ example {a b : ℚ} (h1 : a - b = 4) (h2 : a * b = 1) : (a + b) ^ 2 = 20 :=
   calc
     (a + b) ^ 2 = (a - b) ^ 2 + 4 * (a * b) := by ring
     _ = 4 ^ 2 + 4 * 1 := by rw [h1, h2]
-    _ = 20 := by ring    
+    _ = 20 := by ring
 
 example {r s : ℝ} (h1 : s = 3) (h2 : r + 2 * s = -1) : r = -7 :=
   calc
