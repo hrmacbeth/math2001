4a5a0585da655ce4f68328128a6744c55e374051
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-03-30 15:44:43 -0400

    Update Thu Mar 30 15:44:43 EDT 2023

--- src/Homework/hw7.lean
+++ src/Homework/hw7.lean
@@ -1,6 +1,6 @@
 /- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
 import Mathlib.Data.Real.Basic
-import Math2001.Library.Parity
+import Math2001.Library.ParityModular
 import Math2001.Tactic.Addarith
 import Math2001.Tactic.Numbers
 import Math2001.Tactic.Rel
