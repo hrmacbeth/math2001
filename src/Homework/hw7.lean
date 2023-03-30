bafcd1ac6a6e903d653508047991a1570a61d475
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-03-30 14:25:50 -0400

    Update Thu Mar 30 14:25:50 EDT 2023

--- src/Homework/hw7.lean
+++ src/Homework/hw7.lean
@@ -1,5 +1,6 @@
 /- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
 import Mathlib.Data.Real.Basic
+import Math2001.Library.Parity
 import Math2001.Tactic.Addarith
 import Math2001.Tactic.Numbers
 import Math2001.Tactic.Rel
@@ -8,7 +9,7 @@ import Math2001.Tactic.Take
 set_option linter.unusedVariables false
 set_option pp.unicode.fun true
 set_option push_neg.use_distrib true
-open Function
+open Function Int
 
 
 /-! # Homework 7 -/
