59915f4b8d5c2b1ea584866d608a58dbeecb3ff2
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-03-31 14:13:19 -0400

    Update Fri Mar 31 14:13:19 EDT 2023

--- lakefile.lean
+++ lakefile.lean
@@ -8,5 +8,5 @@ lean_lib Math2001 where
   moreLeanArgs := #["-DwarningAsError=true", "-Dpp.unicode.fun=true"] -- pretty-prints `fun a ↦ b`
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "9458c078999f9d8baf21d559545c17ac9ce8ce4d"
-require autograder from git "https://github.com/robertylewis/cs22-lean-autograder" @ "1c9c67a3d0c3d5d5b159c37e3146323d3b6f4bb1"
+require autograder from git "https://github.com/robertylewis/cs22-lean-autograder" @ "1c6119111649e9c18594be3b3722836025a96e86"
 
