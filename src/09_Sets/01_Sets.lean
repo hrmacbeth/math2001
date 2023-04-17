1beefa37a17a3917f006dd0d867caf2446c518f0
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-04-17 08:31:11 -0400

    Update Mon Apr 17 08:31:11 EDT 2023

--- src/09_Sets/01_Sets.lean
+++ src/09_Sets/01_Sets.lean
@@ -1,10 +1,12 @@
 import Mathlib.Data.Real.Basic
 import Math2001.Library.Parity
+import Math2001.Tactic.Addarith
 import Math2001.Tactic.Numbers
 import Math2001.Tactic.Rel
 import Math2001.Tactic.Take
 
 set_option push_neg.use_distrib true
+open Set
 
 namespace Nat
 
@@ -21,6 +23,24 @@ example : 10 ∉ { n : ℕ | Odd n } := by
   rw [← even_iff_not_odd]
   take 5
   numbers
+
+
+example : { a : ℕ | 4 ∣ a } ⊆ { b : ℕ | 2 ∣ b } := by
+  dsimp [subset_def]
+  intro a ha
+  obtain ⟨k, hk⟩ := ha
+  take 2 * k
+  calc a = 4 * k := hk
+    _ = 2 * (2 * k) := by ring
+
+
+example : ¬ { x : ℝ | 0 ≤ x ^ 2 } ⊆ { t : ℝ | 0 ≤ t } := by
+  dsimp [subset_def]
+  push_neg
+  take -3
+  constructor
+  · numbers
+  · numbers  
 
 /-! # Exercises -/
 
@@ -55,4 +75,30 @@ example : -3 ∈ { x : ℝ | ∀ y : ℝ, x ≤ y ^ 2 } := by
   sorry
 
 example : -3 ∉ { x : ℝ | ∀ y : ℝ, x ≤ y ^ 2 } := by
+  sorry
+
+
+example : { a : ℕ | 20 ∣ a } ⊆ { x : ℕ | 5 ∣ x } := by
+  sorry
+
+example : ¬ { a : ℕ | 20 ∣ a } ⊆ { x : ℕ | 5 ∣ x } := by
+  sorry
+
+
+example : { a : ℕ | 5 ∣ a } ⊆ { x : ℕ | 20 ∣ x } := by
+  sorry
+
+example : ¬ { a : ℕ | 5 ∣ a } ⊆ { x : ℕ | 20 ∣ x } := by
+  sorry
+
+example : { r : ℤ | 3 ∣ r } ⊆ { s : ℤ | 0 ≤ s } := by
+  sorry
+
+example : ¬ { r : ℤ | 3 ∣ r } ⊆ { s : ℤ | 0 ≤ s } := by
+  sorry
+
+example : { m : ℤ | m ≥ 10 } ⊆ { n : ℤ | n ^ 3 - 7 * n ^ 2 ≥ 4 * n } := by
+  sorry
+
+example : ¬ { m : ℤ | m ≥ 10 } ⊆ { n : ℤ | n ^ 3 - 7 * n ^ 2 ≥ 4 * n } := by
   sorry
