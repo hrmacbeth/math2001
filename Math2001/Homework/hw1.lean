96427f68d8dc6bdd21073f94167a3dea24343acc
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2023-09-06 00:22:31 -0400

    Update Wed Sep  6 00:22:31 EDT 2023

--- Math2001/Homework/hw1.lean
+++ Math2001/Homework/hw1.lean
@@ -11,21 +11,21 @@ attribute [-instance] Int.instDivInt_1 Int.instDivInt EuclideanDomain.instDiv Na
 
 
 /- 5 points -/
-theorem problem1 {p q : ℤ} (h1 : p + 4 * q = 1) (h2 : q - 1 = 2) : p = -11 :=
+theorem problem1 {p q : ℤ} (h1 : p + 4 * q = 1) (h2 : q - 1 = 2) : p = -11 := by
   sorry
 
 /- 5 points -/
-theorem problem2 {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 :=
+theorem problem2 {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 := by
   sorry
 
 /- 5 points -/
-theorem problem3 {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 :=
+theorem problem3 {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 := by
   sorry
 
 /- 5 points -/
-theorem problem4 {x : ℚ} : x ^ 2 - 2 * x ≥ -1 :=
+theorem problem4 {x : ℚ} : x ^ 2 - 2 * x ≥ -1 := by
   sorry
 
 /- 5 points -/
-theorem problem5 (a b : ℝ) (h : a ≤ b) : a ^ 3 ≤ b ^ 3 := by
+theorem problem5 (a b : ℝ) (h1 : -b ≤ a) (h2 : a ≤ b) : a ^ 2 ≤ b ^ 2 := by
   sorry
