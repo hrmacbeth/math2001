040b055efe622659a612f26ffc84eb7409ab70a8
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-02-01 03:05:22 +0000

    Update Thu Feb  1 03:05:22 UTC 2024

---
+++ Math2001/Homework/hw2.lean
@@ -0,0 +1,37 @@
+/- Copyright (c) Heather Macbeth, 2023.  All rights reserved. -/
+import Mathlib.Data.Real.Basic
+import Library.Basic
+import AutograderLib
+
+math2001_init
+
+/-! # Homework 2
+
+Don't forget to compare with the text version,
+https://github.com/hrmacbeth/math2001/wiki/Homework-2,
+for clearer statements and any special instructions. -/
+
+
+@[autograded 4]
+theorem problem1 {x : ℚ} (h1 : x ^ 2 = 4) (h2 : 1 < x) : x = 2 := by
+  sorry
+
+@[autograded 5]
+theorem problem2 {s : ℚ} (h1 : 3 * s ≤ -6) (h2 : 2 * s ≥ -4) : s = -2 := by
+  sorry
+
+@[autograded 2]
+theorem problem3 {a b : ℝ} (h : a = 2 - b) : a + b = 2 ∨ a + b = 8 := by
+  sorry
+
+@[autograded 4]
+theorem problem4 {t : ℚ} (h : t = -2 ∨ t = 3) : t ^ 2 - t - 6 = 0 := by
+  sorry
+
+@[autograded 5]
+theorem problem5 {x : ℤ} : 2 * x ≠ 7 := by
+  sorry
+
+@[autograded 5]
+theorem problem6 {t : ℝ} (ht : t ^ 3 = t ^ 2) : t = 1 ∨ t = 0 := by
+  sorry
