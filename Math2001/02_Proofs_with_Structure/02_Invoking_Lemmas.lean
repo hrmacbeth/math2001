d98bbd67375b1d87b751c66fdb0c962d5579f563
Yupo (Luke) Chen <799708+lukechen526@users.noreply.github.com>
2024-02-15 23:35:17 +0000

    completed 2.2

--- Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
+++ Math2001/02_Proofs_with_Structure/02_Invoking_Lemmas.lean
@@ -12,7 +12,10 @@ example {x : ℚ} (hx : 3 * x = 2) : x ≠ 1 := by
     _ < 1 := by numbers
 
 example {y : ℝ} : y ^ 2 + 1 ≠ 0 := by
-  sorry
+  apply ne_of_gt
+  calc
+    y ^ 2 + 1 ≥ 1 := by extra
+    _ > 0 := by numbers
 
 example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a ^ 2 = 0 := by
   apply le_antisymm
@@ -26,7 +29,21 @@ example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a ^ 2 = 0 := by
 
 
 example {m : ℤ} (hm : m + 1 = 5) : 3 * m ≠ 6 := by
-  sorry
+  apply ne_of_gt
+  calc
+    3 * m = 3 * (m + 1) - 3 := by ring
+    _ = 3 * 5 - 3 := by rw [hm]
+    _ = 12 := by ring
+    _ > 6 := by numbers
 
 example {s : ℚ} (h1 : 3 * s ≤ -6) (h2 : 2 * s ≥ -4) : s = -2 := by
-  sorry
+  apply le_antisymm
+  calc
+    s = 3 * s / 3 := by ring
+    _ ≤ -6 / 3 := by rel [h1]
+    _ = -2 := by ring
+
+  calc
+    s ≥ 2 * s / 2 := by ring
+    _ ≥ -4 / 2 := by rel [h2]
+    _ = -2 := by ring
