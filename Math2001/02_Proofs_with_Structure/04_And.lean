e6d6210e206531b9c8b4fe9baad79b025c156fa7
Yupo "Luke" Chen <799708+lukechen526@users.noreply.github.com>
2024-02-16 21:41:57 +0000

    completed 2.4

--- Math2001/02_Proofs_with_Structure/04_And.lean
+++ Math2001/02_Proofs_with_Structure/04_And.lean
@@ -20,7 +20,9 @@ example {p : ℚ} (hp : p ^ 2 ≤ 8) : p ≥ -5 := by
       p ^ 2 ≤ 9 := by addarith [hp]
       _ = 3 ^ 2 := by numbers
     numbers
-  sorry
+  obtain ⟨h1, h2⟩ := hp'
+  calc  p ≥ -3 := by addarith [h1]
+        _ ≥ -5 := by numbers
 
 example {a b : ℝ} (h1 : a - 5 * b = 4) (h2 : b + 2 = 3) : a = 9 ∧ b = 1 := by
   constructor
@@ -49,29 +51,92 @@ example {a b : ℝ} (h1 : a ^ 2 + b ^ 2 = 0) : a = 0 ∧ b = 0 := by
       a ^ 2 ≤ a ^ 2 + b ^ 2 := by extra
       _ = 0 := by rw [h1]
     extra
-  sorry
+
+  have h3 : b ^ 2 = 0
+  · apply le_antisymm
+    calc
+      b ^ 2 ≤ a ^ 2 + b ^ 2 := by extra
+      _ = 0 := by rw [h1]
+    extra
+
+  constructor
+  · cancel 2 at h2
+  . cancel 2 at h3
 
 /-! # Exercises -/
 
 
 example {a b : ℚ} (H : a ≤ 1 ∧ a + b ≤ 3) : 2 * a + b ≤ 4 := by
-  sorry
+  obtain ⟨h1, h2⟩ := H
+  calc
+    2 * a + b = a + (a + b) := by ring
+            _ ≤ 1 + 3 := by rel [h1, h2]
+            _ = 4 := by numbers
 
 example {r s : ℝ} (H : r + s ≤ 1 ∧ r - s ≤ 5) : 2 * r ≤ 6 := by
-  sorry
+  obtain ⟨h1, h2⟩ := H
+  calc
+    2 * r = (r + s) + (r - s) := by ring
+        _ ≤ 1 + 5 := by rel [h1, h2]
+        _ = 6 := by numbers
 
 example {m n : ℤ} (H : n ≤ 8 ∧ m + 5 ≤ n) : m ≤ 3 := by
-  sorry
+  obtain ⟨h1, h2⟩ := H
+  calc
+    m ≤ n - 5 := by addarith [h2]
+    _ ≤ 8 - 5 := by addarith [h1]
+    _ = 3 := by numbers
 
 example {p : ℤ} (hp : p + 2 ≥ 9) : p ^ 2 ≥ 49 ∧ 7 ≤ p := by
-  sorry
+  have hp' : p ≥ 7 := by addarith [hp]
+  constructor
+  · calc
+      p ^ 2 ≥ 7 ^ 2 := by rel [hp']
+          _ = 49 := by numbers
+  · exact hp'
+
 
 example {a : ℚ} (h : a - 1 ≥ 5) : a ≥ 6 ∧ 3 * a ≥ 10 := by
-  sorry
+  have h' : a ≥ 6 := by addarith [h]
+  constructor
+  · exact h'
+  · calc
+      3 * a ≥ 3 * 6 := by rel [h']
+          _ = 18 := by numbers
+          _ ≥ 10 := by numbers
 
 example {x y : ℚ} (h : x + y = 5 ∧ x + 2 * y = 7) : x = 3 ∧ y = 2 := by
-  sorry
+  obtain ⟨h1, h2⟩ := h
+  have h3 : y = 2 :=
+    calc
+      y = (x + 2 * y) - (x + y) := by ring
+        _ = 7 - 5 := by rw [h1, h2]
+        _ = 2 := by ring
+
 
 example {a b : ℝ} (h1 : a * b = a) (h2 : a * b = b) :
     a = 0 ∧ b = 0 ∨ a = 1 ∧ b = 1 := by
-  sorry
+
+  have h3 : a * (b - 1) = 0 := by
+    calc
+      a * (b - 1) = a * b - a := by ring
+              _ = 0 := by addarith [h1]
+
+  have h4 := eq_zero_or_eq_zero_of_mul_eq_zero h3
+  obtain h5 | h6 := h4
+  · left
+    constructor
+    . apply h5
+    · calc
+        b = a * b := by rw [h2]
+        _ = 0 * b := by rw [h5]
+        _ = 0 := by ring
+  · right
+    constructor
+    · have h7 :  b = 1 := by addarith [h6]
+      calc
+        a = a * 1 := by ring
+        _ = a * b := by rw [h7]
+        _ = b := by rw [h2]
+        _ = 1 := by addarith [h6]
+    · addarith [h6]
