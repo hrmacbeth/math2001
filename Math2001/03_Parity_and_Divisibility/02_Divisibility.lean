441ff61c2d9d7ed0532a195387ee870f7dfec003
Yupo "Luke" Chen <799708+lukechen526@users.noreply.github.com>
2024-02-18 22:07:53 +0000

    completed 3.2

--- Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
+++ Math2001/03_Parity_and_Divisibility/02_Divisibility.lean
@@ -12,7 +12,9 @@ example : (11 : ℕ) ∣ 88 := by
 
 
 example : (-2 : ℤ) ∣ 6 := by
-  sorry
+  dsimp [(· ∣ ·)]
+  use -3
+  numbers
 
 example {a b : ℤ} (hab : a ∣ b) : a ∣ b ^ 2 + 2 * b := by
   obtain ⟨k, hk⟩ := hab
@@ -23,10 +25,20 @@ example {a b : ℤ} (hab : a ∣ b) : a ∣ b ^ 2 + 2 * b := by
 
 
 example {a b c : ℕ} (hab : a ∣ b) (hbc : b ^ 2 ∣ c) : a ^ 2 ∣ c := by
-  sorry
+  dsimp [(· ∣ ·)]
+  obtain ⟨s, hs⟩ := hab
+  obtain ⟨t, ht⟩ := hbc
+  use s^2 * t
+  calc
+    c = b^2 * t := ht
+    _ = (a * s)^2 * t := by rw [hs]
+    _ = a^2 * (s^2 * t) := by ring
 
 example {x y z : ℕ} (h : x * y ∣ z) : x ∣ z := by
-  sorry
+  obtain ⟨s, hs⟩ := h
+  use s * y
+  rw [hs]
+  ring
 
 example : ¬(5 : ℤ) ∣ 12 := by
   apply Int.not_dvd_of_exists_lt_and_lt
@@ -51,16 +63,26 @@ example {a b : ℕ} (hb : 0 < b) (hab : a ∣ b) : a ≤ b := by
 
 
 example {a b : ℕ} (hab : a ∣ b) (hb : 0 < b) : 0 < a := by
-  sorry
+  obtain ⟨k, hk⟩ := hab
+  have H1 :=
+    calc
+      0 < b := hb
+      _ = a * k := hk
+  cancel k at H1
 
 /-! # Exercises -/
 
 
 example (t : ℤ) : t ∣ 0 := by
-  sorry
+  use 0
+  ring
 
 example : ¬(3 : ℤ) ∣ -10 := by
-  sorry
+  apply Int.not_dvd_of_exists_lt_and_lt
+  use -4
+  constructor
+  · numbers
+  · numbers
 
 example {x y : ℤ} (h : x ∣ y) : x ∣ 3 * y - 4 * y ^ 2 := by
   sorry
@@ -75,10 +97,26 @@ example {k l m : ℤ} (h1 : k ∣ l) (h2 : l ^ 3 ∣ m) : k ^ 3 ∣ m := by
   sorry
 
 example {p q r : ℤ} (hpq : p ^ 3 ∣ q) (hqr : q ^ 2 ∣ r) : p ^ 6 ∣ r := by
-  sorry
+  obtain ⟨s, hs⟩ := hpq
+  obtain ⟨t, ht⟩ := hqr
+  use s^2 * t
+  calc
+    r = q^2 * t := ht
+    _ = (p^3 * s)^2 * t := by rw [hs]
+    _ = p^6 * (s^2 * t) := by ring
 
 example : ∃ n : ℕ, 0 < n ∧ 9 ∣ 2 ^ n - 1 := by
-  sorry
+  use 6
+  constructor
+  · numbers
+  · use 7
+    numbers
 
 example : ∃ a b : ℤ, 0 < b ∧ b < a ∧ a - b ∣ a + b := by
-  sorry
+  use 3, 2
+  constructor
+  · numbers
+  · constructor
+    · numbers
+    · use 5
+      numbers
