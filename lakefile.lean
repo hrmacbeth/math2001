149fd8519924ff12cdebeb9127d741d04c04a663
Heather Macbeth <25316162+hrmacbeth@users.noreply.github.com>
2024-09-04 01:39:29 -0400

    Update Wed Sep  4 01:39:29 EDT 2024

--- lakefile.lean
+++ lakefile.lean
@@ -29,4 +29,4 @@ but currently only Lean core options can be set in lakefile
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4" @ s!"v{Lean.versionString}"
 require Duper from git "https://github.com/hrmacbeth/duper" @ "main"
-require autograder from git "https://github.com/robertylewis/lean4-autograder-main" @ "master"
+require autograder from git "https://github.com/robertylewis/lean4-autograder-main" @ "864b34ce06d8536aec0c38e57448c17d1f83572a"
